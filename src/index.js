eb7cedf50df5ecd7da6b0c4e0b20568f11aece90
Jessica Poe <jesspoe00@gmail.com>
2019-05-12 11:17:29 -0700

    deploying to heroku

--- src/index.js
+++ src/index.js
@@ -5,14 +5,10 @@ import "mdbreact/dist/css/mdb.css";
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
-
 import * as serviceWorker from './serviceWorker';
 
 
-ReactDOM.render(<BrowserRouter>
-  <App />
-</BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
