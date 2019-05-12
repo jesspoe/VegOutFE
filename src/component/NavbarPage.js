95c0ce73de34251ead4aa33b722a023b8b6bdc18
Jessica Poe <jesspoe00@gmail.com>
2019-05-12 11:43:35 -0700

    deploying to heroku

--- src/component/NavbarPage.js
+++ src/component/NavbarPage.js
@@ -4,6 +4,7 @@ import {
   MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavbarToggler, MDBCollapse, MDBDropdown,
   MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon
 } from "mdbreact";
+import Navbar from 'react-bootstrap/Navbar'
 
 
 class NavbarPage extends Component {
@@ -17,39 +18,16 @@ class NavbarPage extends Component {
 
   render() {
     return (
-      <div>
-      <MDBNavbar color="white" expand="md">
-        <MDBNavbarBrand>
-          <strong className="black-text">VegOut!</strong>
-        </MDBNavbarBrand>
-        <MDBNavbarToggler onClick={this.toggleCollapse} />
-        <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
-          <MDBNavbarNav left>
-            <MDBNavItem >
-              <Nav.Link className="black-text" href="/">Home</Nav.Link>
-            </MDBNavItem>
-            <MDBNavItem >
-              <Nav.Link className="black-text" href="/groups">Groups</Nav.Link>
-            </MDBNavItem>
-            <MDBNavItem>
-              <Nav.Link className="black-text" href="/resources">Resources</Nav.Link>
-            </MDBNavItem>
-          </MDBNavbarNav>
-          <MDBNavbarNav right>
-            <MDBNavItem>
-              <MDBDropdown>
-                <MDBDropdownToggle nav caret>
-                  <span className="black-text" >More</span>
-                </MDBDropdownToggle>
-                <MDBDropdownMenu className="dropdown-default" right>
-                  <MDBDropdownItem href="/logout">Log Out</MDBDropdownItem>
-                </MDBDropdownMenu>
-              </MDBDropdown>
-            </MDBNavItem>
-          </MDBNavbarNav>
-        </MDBCollapse>
-      </MDBNavbar>
-      </div>
+    
+<Navbar bg="light" variant="light">
+<Navbar.Brand href="#home">VegOut!</Navbar.Brand>
+<Nav className="mr-auto">
+  <Nav.Link href="/">Home</Nav.Link>
+  <Nav.Link href="/groups">Groups</Nav.Link>
+  <Nav.Link href="/resources">Resources</Nav.Link>
+</Nav>
+<Nav.Link href="/logout">Logout</Nav.Link>
+</Navbar>
     );
   }
 }
@@ -57,3 +35,37 @@ class NavbarPage extends Component {
 export default NavbarPage;
 
 
+
+{/* <div>
+<MDBNavbar color="white" expand="md">
+  <MDBNavbarBrand>
+    <strong className="black-text">VegOut!</strong>
+  </MDBNavbarBrand>
+  <MDBNavbarToggler onClick={this.toggleCollapse} />
+  <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+    <MDBNavbarNav left>
+      <MDBNavItem >
+        <Nav.Link className="black-text" href="/">Home</Nav.Link>
+      </MDBNavItem>
+      <MDBNavItem >
+        <Nav.Link className="black-text" href="/groups">Groups</Nav.Link>
+      </MDBNavItem>
+      <MDBNavItem>
+        <Nav.Link className="black-text" href="/resources">Resources</Nav.Link>
+      </MDBNavItem>
+    </MDBNavbarNav>
+    <MDBNavbarNav right>
+      <MDBNavItem>
+        <MDBDropdown>
+          <MDBDropdownToggle nav caret>
+            <span className="black-text" >More</span>
+          </MDBDropdownToggle>
+          <MDBDropdownMenu className="dropdown-default" right>
+            <MDBDropdownItem href="/logout">Log Out</MDBDropdownItem>
+          </MDBDropdownMenu>
+        </MDBDropdown>
+      </MDBNavItem>
+    </MDBNavbarNav>
+  </MDBCollapse>
+</MDBNavbar>
+</div> */}
